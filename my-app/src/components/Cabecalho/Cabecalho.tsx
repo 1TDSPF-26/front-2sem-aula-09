c8e37baab03f3169e47d85e82103dd12fa8210f1
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-01 08:52:44 -0300

    feat: Correção tarefa de casa, links modulares e criação de rotas extras.

--- my-app/src/components/Cabecalho/Cabecalho.tsx
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -4,8 +4,6 @@ export default function Cabecalho() {
 
     return (
         <header className="bg-blue-500   text-white font-sans flex-row p-4">
-        
-
             <h1>Meu componente</h1>
             <Menu/>
         </header>
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -11,6 +11,8 @@ import Home from './routes/Home/index.tsx';
 import Produtos from './routes/Produtos/index.tsx';
 import EditarProdutos from './routes/EditarProdutos/index.tsx';
 import Error from './routes/Error/index.tsx';
+import Conteudo from './routes/Conteudo/index.tsx';
+
 
 //Criando uma lista de objetos de rotas:
 const router = createBrowserRouter([
@@ -19,6 +21,7 @@ const router = createBrowserRouter([
     {path: "/produtos", element:<Produtos/>},
     {path:"/editar-produtos/:id", element:<EditarProdutos/>},
     //"/editar-produtos/:id" é o padrão para o react-router, se não for informado, ele vai procurar o padrão "editar-produtos" e vai procurar
+    { path: "/conteudo", element: <Conteudo/> },
   ]},
   
 ]);
