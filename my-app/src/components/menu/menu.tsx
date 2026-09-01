6f6688b9e5dc37e358e385e2dab975d3cd1761dc
Gabriel <gabrielrazod9j@gmail.com>
2026-09-01 08:52:32 -0300

    feat: correção tarefa de casa e criação de rotas

--- my-app/src/components/menu/menu.tsx
+++ my-app/src/components/menu/menu.tsx
@@ -1,29 +1,12 @@
 import { Link } from "react-router";
-//Mecanismo modular: cada item do menu é apenas um objeto nesse array.
-//Para adicionar/remover uma página do menu, basta editar esta lista,
-//sem precisar mexer no JSX abaixo.
-//molde dos itens do menu:
-type ItemMenu = {
-  label: string;
-  to: string;
-};
-
-//o texto exibido no menu e o link para onde ele vai, são definidos aqui:
-const itensMenu: ItemMenu[] = [
-  { label: "Home", to: "/" },
-  { label: "Produtos", to: "/produtos" },
-  { label: "Conteúdo", to: "/conteudo" },
-];
 
 export default function Menu() {
   return (
     <nav>
       <ul>
-        {itensMenu.map((item) => (
-          <li key={item.to}>
-            <Link to={item.to}>{item.label}</Link>
-          </li>
-        ))}
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/produtos">Produtos</Link></li>
+        <li><Link to="/conteudo">Conteudo</Link></li>
       </ul>
     </nav>
   );
