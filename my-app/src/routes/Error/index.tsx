c93b61b4fea85ac9565e90470e569ad07e7dc9be
Erick488-maker <erickmenezes658@gmail.com>
2026-09-01 09:11:48 -0300

    fix: routes error

--- my-app/src/routes/Error/index.tsx
+++ my-app/src/routes/Error/index.tsx
@@ -1,12 +1,7 @@
 export default function Error() {
-<<<<<<< HEAD
- return <h1>Erro 404 - Página não encontrada</h1>
-}
-=======
   return (
     <main>
         <h2>404 - Página não encontrada</h2>
     </main>
   )
 }
->>>>>>> origin/feature/criar-boiler-plate-pf0670
